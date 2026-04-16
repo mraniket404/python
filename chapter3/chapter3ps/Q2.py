28f0e0b31da8c8ef8a0ff89c607ad592828322ad
mraniket404 <aniketgosavi471@gmail.com>
2026-04-16 21:10:22 +0530

    chapter3 complte

---
+++ chapter3/chapter3ps/Q2.py
@@ -0,0 +1,9 @@
+# Simple replace() example
+message = "Hello dear, how are you?"
+
+# Replace "dear" with "John"
+new_message = message.replace('dear', 'John').replace('how are you?', 'i am fine')
+
+
+print("Original:", message)
+print("After replace:", new_message)
