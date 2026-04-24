14a911fcbcfdc46acbf85197014ce2d1c39b2bdb
mraniket404 <aniketgosavi471@gmail.com>
2026-04-24 10:41:39 +0530

    chapter13ps

---
+++ chapter13/chapter13ps/Q2.py
@@ -0,0 +1,5 @@
+name = input("Enter your name: ")
+mark = int(input("Enter your mark: "))
+phone = input("Enter your phone number: ")
+
+print("Name: {}, Mark: {}, Phone: {}".format(name, mark, phone))
